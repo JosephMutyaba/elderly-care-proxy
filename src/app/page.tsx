c97f9bafb3d1c1fa76b2ab0b7d1292d8ce387a2e
JosephMutyaba <josephsteinson@gmail.com>
2024-10-29 17:20:09 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>YOOOOOOOOOOOOOOOOOO!</p><br />
+      <h1>CHRIST FOREVER!!!!!!!!!!!!!!!!</h1>
+    </div>
+  );
+}
